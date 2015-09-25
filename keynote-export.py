8184a4881d4962a9baf938e734042672ed031193
John McKenzie <jmckind@gmail.com>
2015-09-25 17:14:16 -0500

    Add additional constants

--- keynote-export.py
+++ keynote-export.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from AppKit import NSURL
+from AppKit import NSURL, NSMutableDictionary
 from ScriptingBridge import SBApplication
 import sys
 
@@ -22,6 +22,39 @@ EXPORT_FORMAT = {
     'Kkey': 0x4B6B6579, # Keynote 09
 }
 
+IMAGE_FORMATS = {
+    'Kifj': 0x4B69666A, # JPEG
+    'Kifp': 0x4B696670, # PNG
+    'Kift': 0x4B696674, # TIFF
+}
+
+MOVIE_FORMATS = {
+    'Kmf3': 0x4B6D6633, # 360p
+    'Kmf5': 0x4B6D6635, # 540p
+    'Kmf7': 0x4B6D6637, # 720p
+}
+
+PRINT_WHAT = {
+    'Kpwi': 0x4B707769, # individual slides
+    'Kpwn': 0x4B70776E, # slides with notes
+    'Kpwh': 0x4B707768, # handouts
+}
+
+EXPORT_PROPERTIES = {
+    'Kxic': 0x4B786963, # compressed image quality, ranging from 0.0 to 1.0
+    'Kxif': 0x4B786966, # format for resulting images
+    'Kxmf': 0x4B786D66, # format for exported movie
+    'Kxpw': 0x4B787077, # choose whether to include notes, etc.
+    'Kxpa': 0x4B787061, # print each stage of builds
+    'Kxps': 0x4B787073, # include skipped slides
+    'Kxpb': 0x4B787062, # add borders around slides
+    'Kxpn': 0x4B78706E, # include slide numbers
+    'Kxpd': 0x4B787064, # include date
+    'Kxkf': 0x4B786B66, # export in raw KPF
+    'KxPW': 0x4B785057, # password
+    'KxPH': 0x4B785048, # password hint
+}
+
 if len(sys.argv) < 2:
     print "usage: %s <keynote-file>" % sys.argv[0]
     sys.exit(-1)
@@ -30,7 +63,8 @@ if len(sys.argv) < 2:
 keynote_file = sys.argv[1]
 to_file = NSURL.fileURLWithPath_(keynote_file.split('.key')[0] + '.pdf')
 as_format = EXPORT_FORMAT['Kpdf']
-with_properties = None
+with_properties = NSMutableDictionary.dictionaryWithDictionary_({
+})
 
 if DEBUG:
     print("   KEYNOTE_FILE: %s" % keynote_file)
